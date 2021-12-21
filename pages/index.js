4ebccd5f1d9669929e3efc241ba08e3c96c1c8a9
cachemonet0x0CF6619 <90769841+cachemonet0x0CF6619@users.noreply.github.com>
2021-12-20 19:24:53 -0600

    fix: style buttons

--- pages/index.js
+++ pages/index.js
@@ -150,6 +150,7 @@ export default function Home() {
           </div>
           <div className="mt-10 w-full max-w-xs">
             <button
+              className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
               className=""
               onClick={() => connectWallet('injected')} >
               Connect
@@ -397,7 +398,7 @@ export default function Home() {
         </div>
         <div className="mt-10 w-full max-w-xs">
           <button
-            className=""
+            className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
             disabled={isClaiming}
             onClick={() => mintNFT()}
           >
